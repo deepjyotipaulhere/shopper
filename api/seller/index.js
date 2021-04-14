f9652b3741f2d3836b94986bf13aba2ab3881638
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2021-04-14 23:03:46 +0530

    more seller and shop

--- api/seller/index.js
+++ api/seller/index.js
@@ -1,18 +1,22 @@
 import express from 'express'
 import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const conn = new pg.Pool({
-    // connectionString: process.env.POSTGRES_URL
-    user: 'postgres',    
-    password: '1234',
+    user: process.env.USER,
+    password: process.env.PASS,
     database: process.env.DB
 })
 let router = express.Router()
 
 router.post("/register", (req, res) => {
-    const { name, lat, lng, categories } = req.body
-    conn.query("SELECT * FROM seller").then((res1, err) => {
-        res.json(res1.rows)
+    const { name, email, password, mobile, lat, lng, address, photo } = req.body
+    conn.query(`INSERT INTO seller(seller_name,email,password,mobile,location,address,photo) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING "ID"`, [name, email, password, mobile, lat + "," + lng, address, photo]).then((res1, err) => {
+        res.json(res1.rows[0])
+    }).catch(err => {
+        res.status(500).json(err)
     })
 })
 
--- api/shop/index.js
+++ api/shop/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import pg from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const conn = new pg.Pool({
-    user: 'postgres',
-    password: '1234',
+    user: process.env.USER,
+    password: process.env.PASS,
     database: process.env.DB
 })
 const router = express.Router()
@@ -14,23 +17,43 @@ router.post("/createcategory", (req, res) => {
     conn.query(`INSERT INTO shop_category(type) VALUES($1) RETURNING "ID",type`, [name]).then(response => {
         res.json(response.rows[0])
     }).catch(err => {
-        res.json(500).send(err)
+        res.status(500).send(err)
     })
 })
 
 router.get("/getcategory", (req, res) => {
     conn.query("SELECT * FROM shop_category").then(response => {
         res.json(response.rows)
-    }).catch(err=>{
+    }).catch(err => {
         res.status(500).json(err)
     })
 })
 
 
 router.post("/register", (req, res) => {
-    const { name, lat, lng, categories } = req.body
-    conn.query("SELECT * FROM shop").then((res1, err) => {
-        res.json(res1.rows)
+    const { name, lat, lng, categories, established, isonline, address, sellerid } = req.body
+    conn.query(`WITH rows AS(INSERT INTO shop(name, location, category, established, isonline, active, address)
+        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING "ID",name) INSERT INTO seller_shop(seller_id,shop_id,joined_on) 
+        SELECT '${sellerid}',"ID",CURRENT_TIMESTAMP FROM rows RETURNING SELLER_ID,SHOP_ID`,
+        [name, lat + "," + lng, categories, established, isonline, true, address]
+    ).then(response => {
+        res.json(response.rows[0])
+    }).catch(err => {
+        res.status(500).send(err)
+    })
+})
+
+
+router.post("/addproduct", (req, res) => {
+    const { name, price, date, shopid, picture } = req.body
+    console.log(req.body);
+
+    // TODO Insert pictures in a folder
+    
+    conn.query(`INSERT INTO product(name,price,date,shop_id,picture) VALUES($1,$2,$3,$4,$5) RETURNING "ID"`, [name, price, date, shopid, picture]).then(response => {
+        res.json(response.rows[0])
+    }).catch(err => {
+        res.status(500).send(err)
     })
 })
 
